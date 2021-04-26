3cae51784b1d7800adefd33b2195ef0b3afc7dc5
夜雨 <imyeyu@qq.com>
2021-04-26 19:14:55 +0800

    多根节点树形组件

---
+++ src/main/java/net/imyeyu/betterfx/extend/XTreeView.java
@@ -0,0 +1,30 @@
+package net.imyeyu.betterfx.extend;
+
+import javafx.collections.ObservableList;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+
+/**
+ * 不显示根节点的树形结构，实现多个根节点
+ *
+ * 夜雨 创建于 2021/4/26 01:34
+ */
+public class XTreeView<T> extends TreeView<T> {
+
+	private final TreeItem<T> dummyRoot = new TreeItem<>();
+
+	public XTreeView() {
+		dummyRoot.setExpanded(true);
+		setRoot(dummyRoot);
+		setShowRoot(false);
+	}
+
+	@SafeVarargs
+	public final void setRoots(TreeItem<T>... roots) {
+		dummyRoot.getChildren().addAll(roots);
+	}
+
+	public ObservableList<TreeItem<T>> getRoots() {
+		return dummyRoot.getChildren();
+	}
+}
